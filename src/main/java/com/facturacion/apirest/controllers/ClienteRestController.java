d75ce333ca6467219062c8bda04dd7cf5ec1823a
Alex Acevedo <alex.acpe@outlook.com>
2023-02-02 00:24:32 -0500

    Cors para compartir los recursos con el front

--- src/main/java/com/facturacion/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/facturacion/apirest/controllers/ClienteRestController.java
@@ -3,12 +3,14 @@ package com.facturacion.apirest.controllers;
 import com.facturacion.apirest.models.entity.Cliente;
 import com.facturacion.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://localhost:4200"})//autorizar a la url del front para comartir recursos
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
